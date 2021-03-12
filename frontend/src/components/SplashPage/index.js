ef6348a4d18f67ad749bb6936efc9898cb125955
james <monaco.james18@gmail.com>
2021-03-11 22:06:43 -0600

    added styling for splash page

--- frontend/src/components/SplashPage/index.js
+++ frontend/src/components/SplashPage/index.js
@@ -6,7 +6,7 @@ function SplashPage () {
     return (
         <div className="outer__div_container">
             <div className="inner_div_container">
-                <h1>Welcome to Revival, <a href="/login">login</a> to find your perfect cup of coffee</h1>
+                <h1 className="text__h1_container">Welcome to Revival, <a href="/login">login</a> to find your perfect cup of coffee</h1>
             </div>
         </div>
     )
