b027e6432ca41ff6969a9d0ae219099baa05b2ed
james <monaco.james18@gmail.com>
2021-03-11 21:36:15 -0600

    added react for splash page

---
+++ frontend/src/components/SplashPage/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./splash.css"
+
+function SplashPage () {
+
+    return (
+        <div className="outer__div_container">
+            <div className="inner_div_container">
+                <h1>Welcome to Revival, <a href="/login">login</a> to find your perfect cup of coffee</h1>
+            </div>
+        </div>
+    )
+};
+
+export default SplashPage;
