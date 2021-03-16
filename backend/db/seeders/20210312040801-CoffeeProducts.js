1ab09bd74c5d411f066b58b5c8369d2b7c65b94e
james <monaco.james18@gmail.com>
2021-03-16 18:52:30 -0500

    took away some comments

--- backend/db/seeders/20210312040801-CoffeeProducts.js
+++ backend/db/seeders/20210312040801-CoffeeProducts.js
@@ -224,26 +224,9 @@ module.exports = {
           updatedAt: new Date(),
         },
     ])
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkInsert('People', [{
-        name: 'John Doe',
-        isBetaMember: false
-      }], {});
-    */
   },
 
   down: (queryInterface, Sequelize) => {
     return queryInterface.bulkDelete('Products', null, {});
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkDelete('People', null, {});
-    */
   }
 };
