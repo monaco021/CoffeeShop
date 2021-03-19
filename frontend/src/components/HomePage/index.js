a38052cfcdc3e4b52aab000c2949abeef4100f22
james <monaco.james18@gmail.com>
2021-03-18 19:28:54 -0500

    added products to homepage, need to fix db error with the price

--- frontend/src/components/HomePage/index.js
+++ frontend/src/components/HomePage/index.js
@@ -3,20 +3,42 @@ import { useSelector, useDispatch } from "react-redux";
 import { fetchCoffee } from "../../store/homepage"
 //using NavLink so users can click on product and go to its page 
 import { NavLink } from "react-router-dom";
+import plant from "./plant2 (2).jpg"
 import "./homePage.css"
 
 
 
 const ProductListings = () => {
     const dispatch = useDispatch();
-    const products = useSelector((state) => state);
+    const products = useSelector((state) => state.product);
     useEffect(() => {
         dispatch(fetchCoffee())
     }, [dispatch])
 
     return(
         <div>
-            <h1>Hello</h1>
+            <h1>Revival Coffee</h1>
+            <div className="plantImage__div_container">
+                <img className="plant__image_container" src={plant} alt="plantPic" />
+            </div>
+            <div>
+                {products.map((product) => {
+                    return (
+                        <div className="homepage_inner__div_container">
+                            <div>
+                                <img className="products__image_container" src={product.imageLink} />
+                            </div>
+                            <div>
+                                {product.name}
+                            </div>
+                            <div>
+                                {`$${product.price}`}
+                            </div>
+                        </div>
+                        
+                    )
+                })}
+            </div>
         </div>
     )
 }
